082bcffe1e057e9f10f28731523aac7c316cacf5
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-07-28 14:02:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,7 @@ function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
     this.jobTitle = jobTitle;
 }
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.jobGreet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-}; 
+Employee.prototype = Object.create(Person.prototype); 
 
 // Do not change code below this line
 window.Person = Person;
